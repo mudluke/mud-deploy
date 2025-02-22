12cbf9e9355c583dcdb9c110954a80c89dfd1946
Luke <luchenqun@qq.com>
2025-02-22 11:14:49 +0800

    add replace

---
+++ replace.js
@@ -0,0 +1,45 @@
+import fs from 'fs-extra';
+
+async function main() {
+  // 读取balances.json文件
+  const balances = await fs.readJson('balances.json');
+
+  // 一次性读取replace.txt
+  const content = await fs.readFile('replace.txt', 'utf8');
+  const lines = content.split('\n');
+
+  let addresses = [];
+  let targetAddress = '';
+
+  // 处理每一行
+  for (const line of lines) {
+    // 处理地址行
+    if (line.startsWith('0x')) {
+      addresses.push(line);
+    }
+
+    if (line == '') {
+      const targetAddress = addresses[addresses.length - 1];
+      for (const address of addresses) {
+        for (const balance of balances) {
+          if (balance.address.toLowerCase() == address.toLowerCase()) {
+            balance.address = targetAddress.toLowerCase();
+          }
+        }
+      }
+      console.log(addresses);
+      addresses = [];
+    }
+  }
+
+  // 写入新文件
+  await fs.writeJson('balances-replaced.json', balances, { spaces: 2 });
+  console.log('替换完成');
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
